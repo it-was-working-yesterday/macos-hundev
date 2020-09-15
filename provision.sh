4ef4e3e924cb37374cbbd6d67fde02a8128de3c4
Ber Var <it.was.working.yesterday@gmail.com>
2020-09-15 08:51:24 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,9 +1,21 @@
 # Get brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# Manually install Citrix workspace
+
 # brew install basics
-brew install git node mas
-brew cask install google-chrome anaconda textmate docker intellij-idea alfred path-finder iterm2 calibre fantastical vlc spotify transmission keka
+brew install git node mas minikube helm
+brew cask install google-chrome anaconda textmate docker intellij-idea alfred forklift lens todoist iterm2 calibre fantastical vlc spotify transmission keka witch\
+                  fritzing soundsource monitorcontrol nordvpn karabiner-elements
+mas install Magnet Xcode
+
+# Licenses to acquire
+ # IntelliJ
+ # alfred
+ # Forklift
+ # Fantastical
+ # witch
+ # soundsource
 
 # get the Hungarian keyboard layout
 git clone https://github.com/it-was-working-yesterday/macos-citrix-hun-key-layout.git
