64db02ca1b57e034e459edb0570f95be19c60453
Ber Var <it.was.working.yesterday@gmail.com>
2021-03-22 18:53:09 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@
 # Manually install Citrix workspace
 
 # brew install basics
-brew install git node mas tree htop fd go bat insect
+brew install git node mas tree htop fd go bat insect nnn
 brew install --cask google-chrome anaconda sublime-text docker intellij-idea alfred forklift todoist iterm2 calibre fantastical vlc spotify transmission keka \
                   fritzing soundsource monitorcontrol nordvpn karabiner-elements oracle-jdk handbrake shotcut darktable 
 mas install Magnet Xcode Amphetamine "Home Assistant" GrandPerspective
